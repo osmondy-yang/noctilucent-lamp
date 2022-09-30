7631dda70d0fb0e17b60e5e08f8f7028d691dede
yangjinhua <osmondy@foxmail.com>
2022-09-30 16:36:49 +0800

    fix 统一格式

---
+++ demo/nio-demo/nio/src/com/study/channel/FileChannelDemo1.java
@@ -0,0 +1,33 @@
+package com.study.channel;
+
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class FileChannelDemo1 {
+    //FileChannel读取数据到buffer中
+    public static void main(String[] args) throws Exception {
+        //创建FileChannel
+        String fileUrl = "/Users/yangjinhua/Downloads/尚硅谷Java NIO课程（2021最新版）/nio-code/atguigu_nio/src/com/atguigu/resource/01.txt";
+        RandomAccessFile aFile = new RandomAccessFile(fileUrl,"rw");
+        FileChannel channel = aFile.getChannel();
+
+        //创建Buffer
+        ByteBuffer buf = ByteBuffer.allocate(1024);
+
+        //读取数据到buffer中
+        int bytesRead = channel.read(buf);
+        while(bytesRead != -1) {
+            // 会乱码
+            System.out.println("读取了："+bytesRead);
+            buf.flip();
+            while(buf.hasRemaining()) {
+                System.out.println((char)buf.get());
+            }
+            buf.clear();
+            bytesRead = channel.read(buf);
+        }
+        aFile.close();
+        System.out.println("结束了");
+    }
+}
